422ebd42f0f375f9f8ddd5b083493560be18c690
Julie Shapovalenko <jools.shapo@gmail.com>
2023-07-12 21:36:25 +0300

    update basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/julieshapo">
+    <BrowserRouter basename="/react-router-shop-example">
       <App />
     </BrowserRouter>
   </React.StrictMode>
